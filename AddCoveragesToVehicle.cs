e0f4dfc111b8518e8be888f3d5468ca2f8b42e62
Vladyslav Lytyvnenko <Vladyslav.lytvynenko22@gmail.com>
2022-06-02 23:01:24 +0300

    s

--- AddCoveragesToVehicle.cs
+++ AddCoveragesToVehicle.cs
@@ -1,111 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace NC_Helper
+﻿namespace NC_Helper
 {
     public class AddCoveragesToVehicle
     {
-        var newList = new List<PCCOVERAGE>();
-        Random rnd = new Random();
-                foreach (Constants.CoverageCode suit in (Constants.CoverageCode[]) Enum.GetValues(typeof(Constants.CoverageCode)))
-                {
-                    var Limit_Type = new Limit_Type[]
-                    {
-                        new Limit_Type()
-                        {
-                            IndexAdjustment = null
-                            ,LimitBasisCd = null
-                            ,ValuationCd = null
-                            ,LimitAppliesToCd = null
-                            ,FormatInteger = rnd.Next(400000, 500000).ToString()
-                            ,FormatCurrencyAmt = 0
-                            ,FormatCurrencyAmtSpecified = false
-                            ,FormatModFactor = 0
-                            ,FormatModFactorSpecified = false
-                            ,FormatPct = 0
-                            ,FormatPctSpecified = false
-                            ,FormatText = null
-                        },
+        //var newList = new List<PCCOVERAGE>();
+        //Random rnd = new Random();
+        //        foreach (Constants.CoverageCode suit in (Constants.CoverageCode[]) Enum.GetValues(typeof(Constants.CoverageCode)))
+        //        {
+        //            var Limit_Type = new Limit_Type[]
+        //            {
+        //                new Limit_Type()
+        //                {
+        //                    IndexAdjustment = null
+        //                    ,LimitBasisCd = null
+        //                    ,ValuationCd = null
+        //                    ,LimitAppliesToCd = null
+        //                    ,FormatInteger = rnd.Next(400000, 500000).ToString()
+        //                    ,FormatCurrencyAmt = 0
+        //                    ,FormatCurrencyAmtSpecified = false
+        //                    ,FormatModFactor = 0
+        //                    ,FormatModFactorSpecified = false
+        //                    ,FormatPct = 0
+        //                    ,FormatPctSpecified = false
+        //                    ,FormatText = null
+        //                },
 
-                        new Limit_Type()
-                        {
-                            IndexAdjustment = null
-                            ,LimitBasisCd = null
-                            ,ValuationCd = null
-                            ,LimitAppliesToCd = null
-                            ,FormatInteger = rnd.Next(100000, 300000).ToString()
-                            ,FormatCurrencyAmt = 0
-                            ,FormatCurrencyAmtSpecified = false
-                            ,FormatModFactor = 0
-                            ,FormatModFactorSpecified = false
-                            ,FormatPct = 0
-                            ,FormatPctSpecified = false
-                            ,FormatText = null
-                        },
-                    };
+        //                new Limit_Type()
+        //                {
+        //                    IndexAdjustment = null
+        //                    ,LimitBasisCd = null
+        //                    ,ValuationCd = null
+        //                    ,LimitAppliesToCd = null
+        //                    ,FormatInteger = rnd.Next(100000, 300000).ToString()
+        //                    ,FormatCurrencyAmt = 0
+        //                    ,FormatCurrencyAmtSpecified = false
+        //                    ,FormatModFactor = 0
+        //                    ,FormatModFactorSpecified = false
+        //                    ,FormatPct = 0
+        //                    ,FormatPctSpecified = false
+        //                    ,FormatText = null
+        //                },
+        //            };
 
-        var Deductible_Type = new Deductible_Type[]
-        {
-                        new Deductible_Type()
-                        {
-                            DeductibleBasisCd = null
-                            ,DeductibleTypeCd = null
-                            ,DeductibleAppliesToCd = null
-                            ,FormatInteger = rnd.Next(10000, 60000).ToString()
-                            ,FormatCurrencyAmt = 0
-                            ,FormatCurrencyAmtSpecified = false
-                            ,FormatModFactor = 0
-                            ,FormatModFactorSpecified = false
-                            ,FormatPct = 0
-                            ,FormatPctSpecified = false
-                            ,FormatText = null
-                        },
-        };
+        //var Deductible_Type = new Deductible_Type[]
+        //{
+        //                new Deductible_Type()
+        //                {
+        //                    DeductibleBasisCd = null
+        //                    ,DeductibleTypeCd = null
+        //                    ,DeductibleAppliesToCd = null
+        //                    ,FormatInteger = rnd.Next(10000, 60000).ToString()
+        //                    ,FormatCurrencyAmt = 0
+        //                    ,FormatCurrencyAmtSpecified = false
+        //                    ,FormatModFactor = 0
+        //                    ,FormatModFactorSpecified = false
+        //                    ,FormatPct = 0
+        //                    ,FormatPctSpecified = false
+        //                    ,FormatText = null
+        //                },
+        //};
 
-        var cov = new PCCOVERAGE()
-        {
-            CoverageCd = suit.ToString()
-            ,
-            IterationNumber = null
-            ,
-            CoverageDesc = null
-            ,
-            Limit = Limit_Type
-            ,
-            Deductible = Deductible_Type
-            ,
-            Option = null
-            ,
-            CurrentTermAmt = rnd.Next(1, 10000)
-            ,
-            CurrentTermAmtSpecified = true
-            ,
-            NetChangeAmt = 0
-            ,
-            NetChangeAmtSpecified = false
-            ,
-            WrittenAmt = 0
-            ,
-            WrittenAmtSpecified = false
-            ,
-            EffectiveDt = null
-            ,
-            ExpirationDt = null
-            ,
-            Rate = 0
-            ,
-            RateSpecified = false
-            ,
-            PremiumBasisCd = null
-            ,
-            TerritoryCd = null
-            ,
-            Form = null
-            ,
-            FloodZoneCd = null
-        };
+        //var cov = new PCCOVERAGE()
+        //{
+        //    CoverageCd = suit.ToString()
+        //    ,
+        //    IterationNumber = null
+        //    ,
+        //    CoverageDesc = null
+        //    ,
+        //    Limit = Limit_Type
+        //    ,
+        //    Deductible = Deductible_Type
+        //    ,
+        //    Option = null
+        //    ,
+        //    CurrentTermAmt = rnd.Next(1, 10000)
+        //    ,
+        //    CurrentTermAmtSpecified = true
+        //    ,
+        //    NetChangeAmt = 0
+        //    ,
+        //    NetChangeAmtSpecified = false
+        //    ,
+        //    WrittenAmt = 0
+        //    ,
+        //    WrittenAmtSpecified = false
+        //    ,
+        //    EffectiveDt = null
+        //    ,
+        //    ExpirationDt = null
+        //    ,
+        //    Rate = 0
+        //    ,
+        //    RateSpecified = false
+        //    ,
+        //    PremiumBasisCd = null
+        //    ,
+        //    TerritoryCd = null
+        //    ,
+        //    Form = null
+        //    ,
+        //    FloodZoneCd = null
+        //};
 
-        newList.Add(cov);
-                }
-}
+        //newList.Add(cov);
+        //    }
+    }
 }
