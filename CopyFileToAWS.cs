b3749406b8925e21b6a6b038a9f0e757f77ef18c
Vladyslav Lytyvnenko <Vladyslav.lytvynenko22@gmail.com>
2022-06-29 19:33:11 +0300

    add

--- CopyFileToAWS.cs
+++ CopyFileToAWS.cs
@@ -35,6 +35,24 @@ namespace NC_Helper
                 await connection.InsertAsync(sampleFile);
             }
 
+            S3FileInfo s3File = new S3FileInfo(_s3Client, ConfigurationHelper.S3SiteInstanceName, sampleFile.Id.ToString().ToLower());
+
+            if (!s3File.Exists)
+            {
+                using (Stream s3Stream = s3File.Create())
+                {
+                    using (MemoryStream ms = new MemoryStream(bytes))
+                    {
+                        if (ms.CanSeek)
+                        {
+                            ms.Seek(0, SeekOrigin.Begin);
+                        }
+
+                        ms.CopyTo(s3Stream);
+                    }
+                }
+            }
+
             return true;
         }
     }
